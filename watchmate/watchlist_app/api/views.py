236dca569c3ffef42979700b43a491aae6a63db4
Nihal <nihal.vish29@gmail.com>
2023-01-26 13:50:51 +0530

    user model and temporary login/logout

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from rest_framework.exceptions import ValidationError
 from rest_framework import status
 from rest_framework import generics
 from rest_framework import viewsets
@@ -14,10 +15,23 @@ from watchlist_app.api.serializers import WatchListSerializer,StreamPlatformSeri
 class ReviewCreate(generics.CreateAPIView):
         serializer_class = ReviewSerializer
 
+        def get_queryset(self):
+            return Review.objects.all()
+
+        
+
         def perform_create(self, serializer):
             pk=self.kwargs.get('pk')
             watchlist=WatchList.objects.get(pk=pk)
-            serializer.save(watchlist=watchlist)
+
+
+            review_user = self.request.user
+            review_queryset = Review.objects.filter(watchlist=watchlist,review_user=review_user)
+
+            if review_queryset.exists():
+                raise ValidationError("You have already reviewed this watchlist")
+
+            serializer.save(watchlist=watchlist,review_user=review_user)
 
 class ReviewList(generics.ListAPIView):
         # queryset=Review.objects.all()
